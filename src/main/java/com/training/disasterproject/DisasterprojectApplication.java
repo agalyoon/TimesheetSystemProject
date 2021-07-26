404ee69f660cc1d1517027453d05e299d59ba738
agalyoon <87434745+agalyoon@users.noreply.github.com>
2021-07-26 13:57:10 -0400

    initial commit

---
+++ src/main/java/com/training/disasterproject/DisasterprojectApplication.java
@@ -0,0 +1,13 @@
+package com.training.disasterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/disasterproject/DisasterprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.disasterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
