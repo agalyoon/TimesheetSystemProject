ea6db7e698bb1db51805f6dfe7e115e76cd2a531
saikatmeem <saikat_mist@yahoo.com>
2021-07-27 14:29:01 -0400

    Implement submit_timecard

--- src/main/java/com/training/disasterproject/repository/TimecardRepository.java
+++ src/main/java/com/training/disasterproject/repository/TimecardRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.training.disasterproject.model.Timecard;
-import com.training.disasterproject.model.User;
 
 @Repository
 public interface TimecardRepository extends JpaRepository<Timecard, Long> {
