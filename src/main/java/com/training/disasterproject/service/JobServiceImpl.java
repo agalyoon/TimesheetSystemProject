d0b12fc731e3e3c61fe84f221f7dcc593f49dc0d
agalyoon <87434745+agalyoon@users.noreply.github.com>
2021-07-28 10:05:47 -0400

    Merge branches

--- src/main/java/com/training/disasterproject/service/JobServiceImpl.java
+++ src/main/java/com/training/disasterproject/service/JobServiceImpl.java
@@ -1,5 +1,61 @@
 package com.training.disasterproject.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.training.disasterproject.model.Job;
+import com.training.disasterproject.repository.JobRepository;
+
+@Service
 public class JobServiceImpl implements JobService {
+	@Autowired
+	private JobRepository jobRepo;
+
+	public List<Job> getAllJobs()
+	{
+		return (List<Job>) jobRepo.findAll();
+	}
+
+	public Optional<Job> get(String code)
+	{
+		return jobRepo.findById(code);
+	}
+
+	public void add(Job job) 
+	{
+		
+		jobRepo.save(job);
+	}
+
+	/**
+	 *  Updates the Job with the given job.code, 
+	 *  making sure Job exists
+	 * @param job
+	 * @throws JobNotFoundException
+	 */
+	public void update(Job job) 
+	{
+		
+		
+		jobRepo.save(job);
+	}
+
+	public void delete(Job job) 
+	{
+		
+		jobRepo.delete(job);
+	}
+
+	
+
+	public void delete(String jobCode)
+	{
+		
+			jobRepo.deleteById(jobCode);
+		
+	}
 
 }
--- src/main/java/com/training/disasterproject/service/MachineServiceImpl.java
+++ src/main/java/com/training/disasterproject/service/MachineServiceImpl.java
@@ -1,5 +1,53 @@
 package com.training.disasterproject.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.training.disasterproject.model.Machine;
+import com.training.disasterproject.model.Site;
+import com.training.disasterproject.model.User;
+import com.training.disasterproject.repository.MachineRepository;
+
+
+@Service
 public class MachineServiceImpl implements MachineService{
+	@Autowired
+	private MachineRepository machineRepo;
+
+	public List<Machine> getAllByMachine(Site site, User user)
+	{
+		return machineRepo.findAllBySiteAndMachine(site, user);
+	}
+
+	
+	public void update(Machine entry) 
+	{
+		
+		machineRepo.save(entry);
+	}
+
+	public void delete(Machine entry)
+	{
+		machineRepo.delete(entry);
+	}
+
+	public void delete(String id) 
+	{
+		
+		machineRepo.deleteById(id);
+	}
+
+	public List<Machine> getAllMachine()
+	{
+		return (List<Machine>) machineRepo.findAll();
+	}
+
+	public Optional<Machine> get(String code)
+	{
+		return machineRepo.findById(code);
+	}
 
 }
