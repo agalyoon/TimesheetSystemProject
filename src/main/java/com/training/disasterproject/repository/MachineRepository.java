b73be4f2028a8db17766242b1f78baa31c149521
saikatmeem <saikat_mist@yahoo.com>
2021-07-27 23:37:22 -0400

    machine service and job service class updated

--- src/main/java/com/training/disasterproject/repository/MachineRepository.java
+++ src/main/java/com/training/disasterproject/repository/MachineRepository.java
@@ -1,9 +1,17 @@
 package com.training.disasterproject.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.training.disasterproject.model.Machine;
+import com.training.disasterproject.model.Site;
+import com.training.disasterproject.model.User;
 
 public interface MachineRepository extends JpaRepository<Machine,String> {
+	@Query("from Machine mc where mc.site =:site and mc.user=:user")
+
+	List<Machine> findAllBySiteAndMachine(Site site, User user);
 
 }
--- src/main/java/com/training/disasterproject/service/SiteServiceImpl.java
+++ src/main/java/com/training/disasterproject/service/SiteServiceImpl.java
@@ -1,5 +1,8 @@
 package com.training.disasterproject.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SiteServiceImpl implements SiteService {
 
 }
